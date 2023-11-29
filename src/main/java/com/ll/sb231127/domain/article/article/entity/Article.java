ce72366c8f69d520e77794860216ff0134d95c0b
thflf <thflf93@gmail.com>
2023-11-29 15:16:50 +0900

    feat: article tag 기능 추가

--- src/main/java/com/ll/sb231127/domain/article/article/entity/Article.java
+++ src/main/java/com/ll/sb231127/domain/article/article/entity/Article.java
@@ -1,6 +1,7 @@
 package com.ll.sb231127.domain.article.article.entity;
 
 import com.ll.sb231127.domain.article.articleComment.ArticleComment;
+import com.ll.sb231127.domain.article.articleTag.entity.ArticleTag;
 import com.ll.sb231127.domain.member.member.entity.Member;
 import com.ll.sb231127.global.jpa.baseEntity.BaseEntity;
 import jakarta.persistence.Entity;
@@ -11,6 +12,7 @@ import lombok.experimental.SuperBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
@@ -28,9 +30,14 @@ public class Article extends BaseEntity {
     private Member author;
     private String title;
     private String body;
+
     @OneToMany(mappedBy = "article", cascade = ALL, orphanRemoval = true)
     @Builder.Default
     private List<ArticleComment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "article", cascade = ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<ArticleTag> tags = new ArrayList<>();
 
     public void addComment(Member commentAuthor, String commentBody) {
         ArticleComment comment = ArticleComment
@@ -45,5 +52,33 @@ public class Article extends BaseEntity {
 
     public void removeComment(ArticleComment comment) {
         comments.remove(comment);
+    }
+
+    public void addTag(String tagContent) {
+        ArticleTag tag = ArticleTag.builder()
+                .article(this)
+                .content(tagContent)
+                .build();
+
+        tags.add(tag);
+    }
+
+    public void addTag(String... tagContents) {
+        for (String tagContent : tagContents) {
+            addTag(tagContent);
+        }
+    }
+
+    public String getTagsStr() {
+        String tagsStr = tags
+                .stream()
+                .map(ArticleTag::getContent)
+                .collect(Collectors.joining(" #"));
+
+        if (tagsStr.isBlank()) {
+            return "";
+        }
+
+        return "#" + tagsStr;
     }
 }
