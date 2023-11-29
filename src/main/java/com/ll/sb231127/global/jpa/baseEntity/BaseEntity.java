620f967d8974bc137914f65980240db7203595aa
thflf <thflf93@gmail.com>
2023-11-29 11:44:06 +0900

    feat: createDate, modifyDate 추가

--- src/main/java/com/ll/sb231127/global/jpa/baseEntity/BaseEntity.java
+++ src/main/java/com/ll/sb231127/global/jpa/baseEntity/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.ll.sb231127.global.jpa.baseEntity;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
@@ -8,11 +9,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) // @CreatedDate, @LastModifiedDate를 사용하기 위해 필요
 @SuperBuilder
 @NoArgsConstructor(access = PROTECTED)
 @Getter
@@ -23,4 +30,8 @@ public class BaseEntity {
     @GeneratedValue(strategy = IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
+    @CreatedDate
+    private LocalDateTime createDate;
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
 }
